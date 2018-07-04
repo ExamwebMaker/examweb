973f879b347d814a20787dc2285e1d992d825ecf
frozen521 <yanni31316@outlook.com>
2018-07-04 12:27:02 +0800

    :bulb: Documenting source code.

--- src/main/resources/static/js/enroll.js
+++ src/main/resources/static/js/enroll.js
@@ -1,4 +1,84 @@
 $(document).ready(function () {
+    $("#genkaoshenghao").click(function () {
+        $.ajax({
+            url: '',
+            type: 'post',
+            dataType: 'json',
+            data: JSON.stringify({
+                "zhaoShengUnit": $("#zhaoshengdanwei1").val(), "examWay": $("#kaoshifangshi").val(),
+                "specialPlan": $("#zhuanxiangjihua").val(),
+                "name": $("#studentname").val(),
+                "nameSpell": $("#studentnamepinyin").val(),
+                "nowSolier": $("#xianyijunren").val(),
+                "nation": $("#kaoshengminzu").val(),
+                "sex": $("input[name=sex]:checked").val(),
+                "marriage": $("#input[name=marriage]:checked").val(),
+                "policy":$("#zhengzhimianmao").val(),
+                "connectAddress":$("#tongxundizhi").val(),
+                "connectPostalcode":$("#youzhengbianma").val(),"nativePlace":$("#kaoshengjiguan1").val()+$("#kaoshengjiguan2").val()+$("#kaoshengjiguan3").val(),
+                "huKouPlace":$("#hukou1").val()+$("#hukou2").val()+$("#hukou3").val(),
+                "huKouDetail":$("#hukouxiangxixinxi").val(),
+                "birthPlace":$("#chushengdi1").val()+$("#chushengdi2").val()+$("#chushengdi3").val(),
+                "filePlace":$("#dangansuozaidi").val(),
+                "fileUnitName":$("#danganmingzi").val(),
+                "fileUnitAddress":$("#dangandizhi").val(),
+                "fileUnitPostalcode":$("#danganyoubian").val(),
+                "rewardAndPunishment":$("#jianglichufen").val(),
+                "wantDepartmentName":$("#baokaoyuanxiao").val(),
+                "wantMajorName":$("#baokaozhuanye").val(),
+                "researchWay":$("#yanjiufangxiang").val(),
+                "learnWay":$("#xuexifangshi").val(),
+                "examSubject":$("#kaoshikemu").val(),
+                "examPointAddress":$("#baokaodianchengshi").val(),
+                "examPointName":$("#baokaodianmingcheng").val(),
+
+
+
+
+
+            }),
+            contentType: 'application/json; charset=UTF-8',
+            timeout: 1000,
+            cache: false
+        })
+            .done(function (data) {
+                console.log("success");
+                alert(data.data)
+
+            })
+            .fail(function () {
+                console.log("error");
+            })
+            .always(function () {
+                console.log("complete");
+            });
+    })
+
+
+    $.ajax({
+        url: 'http://ciiibv.natappfree.cc/commonUser/getSchoolNameByProvince',
+        type: 'post',
+        dataType: 'json',
+        data: JSON.stringify({"province": $("#zhaoshengdanwei1").val()}),
+        contentType: 'application/json; charset=UTF-8',
+        timeout: 1000,
+        cache: false
+    })
+        .done(function (data) {
+            console.log("success");
+            alert(data.data)
+            for (var i = 0; i < data.length; i++) {
+                $("#zhaoshengdanwei2").append("<option value='" + data.data[i] + "'>" + data.data[i] + "</option>");
+            }
+        })
+        .fail(function () {
+            console.log("error");
+        })
+        .always(function () {
+            console.log("complete");
+        });
+
+
     /**
      * @author YangZhengqian
      * @date 2018/6/22
