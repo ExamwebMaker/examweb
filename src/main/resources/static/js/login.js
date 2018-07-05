7e2aec1b8bf1056f3deeb3bca504e1830fe06e61
frozen521 <yanni31316@outlook.com>
2018-07-05 10:58:20 +0800

    :egg: Adding an easter egg.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
                 $("#password").val("")
             }
             $.ajax({
-                url: "http://fwybgp.natappfree.cc/commonUser/login",
+                url: "/commonUser/login",
                 type: 'POST',
                 dataType: 'json',
                 data: JSON.stringify({
@@ -44,7 +44,7 @@ $(document).ready(function () {
                     $.cookie('phone', data.data.phone)
 
 
-                    window.location.href = "/stdcenter"
+                    window.location.href = "center.html"
                 })
                 .fail(function () {
                     alert("用户名或密码错误!")
