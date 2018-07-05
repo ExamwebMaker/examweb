ed63b6741e828777a68622e42a5f18c03085b95c
frozen521 <yanni31316@outlook.com>
2018-07-05 08:59:03 +0800

    :egg: Adding an easter egg.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $("#username").val($.cookie('name'))
+    $("#password").val($.cookie('password'))
     $("#loginbtn").click(function (event) {
 
 
@@ -9,9 +11,16 @@ $(document).ready(function () {
             // $.session.set('password', $("#password").val());
             // // sessionStorage.setItem('name',$('#name').va)
             // window.location.href = "center.html"
+            if ($("#rempass").is(":checked") == true) {
+                $.cookie('name', $("#username").val())
+                $.cookie('password', $("#password").val())
+            }else if ($("#rempass").is(":checked")==false){
 
+                $("#username").val("")
+                $("#password").val("")
+            }
             $.ajax({
-                url: "/commonUser/login",
+                url: "http://fwybgp.natappfree.cc/commonUser/login",
                 type: 'POST',
                 dataType: 'json',
                 data: JSON.stringify({
@@ -32,7 +41,7 @@ $(document).ready(function () {
                     $.cookie('certificateStyle', data.data.certificateStyle)
                     $.cookie('certificateNumber', data.data.certificateNumber)
                     $.cookie('email', data.data.email)
-                    $.cookie('phone',data.data.phone)
+                    $.cookie('phone', data.data.phone)
 
 
                     window.location.href = "/stdcenter"
@@ -44,6 +53,7 @@ $(document).ready(function () {
                 .always(function () {
                     console.log("complete");
                 });
+
         }
         /* Act on the event */
         /* Act on the event */
