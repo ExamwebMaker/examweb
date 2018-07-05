d1fa605ec99ae331d1f0c610eed6c43a47562cde
jessiecaicai@yeah.net <jessiecaicai@yeah.net>
2018-07-05 10:57:07 +0800

    login ok

--- src/main/java/com/examweb/group/controller/CommonManagerController.java
+++ src/main/java/com/examweb/group/controller/CommonManagerController.java
@@ -1,7 +1,9 @@
 package com.examweb.group.controller;
 
 import com.examweb.group.dto.Result;
+import com.examweb.group.entity.Account;
 import com.examweb.group.entity.Examinee;
+import com.examweb.group.service.AccountService;
 import com.examweb.group.service.ExamineeService;
 import com.examweb.group.utils.ResultUtil;
 import com.github.pagehelper.PageInfo;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 /**
@@ -21,10 +24,12 @@ import javax.validation.Valid;
 public class CommonManagerController {
 
     private ExamineeService examineeService;
+    private AccountService accountService;
 
     @Autowired
-    protected CommonManagerController(ExamineeService examineeService){
+    protected CommonManagerController(ExamineeService examineeService,AccountService accountService){
         this.examineeService=examineeService;
+        this.accountService=accountService;
     }
 
     /**
@@ -47,6 +52,46 @@ public class CommonManagerController {
         }
         return ResultUtil.selectError();
     }
+
+    /**
+     * @Description: 用于普通用户登录的接口
+     * @Json: {
+    "name": "string",
+    "password": "string"
+    }
+     * @Date: 2018/7/3
+     * @Return:
+     */
+    @PostMapping("/managerLogin")
+    @CrossOrigin
+    public Result managerLogin(@RequestBody Account account, HttpSession session) {
+        String name=account.getName();
+        String password=account.getPassword();
+        if (name==null||name.trim().isEmpty()){
+            return ResultUtil.fail("输入用户名不能为空");
+        }
+        if (password==null||password.trim().isEmpty()){
+            return ResultUtil.fail("输入密码不能为空");
+        }
+        try {
+            if (accountService.checkManagerIsExist(name,password)==0){
+                Account accountGet=accountService.getManagerByNameAndPassword(name,password);
+                //System.out.print(accountGet.toString());
+                session.setAttribute("account",accountGet);
+                //session.setAttribute("accountId",accountGet.getId());
+                //session.setAttribute("name",accountGet.getName());
+                //session.setAttribute("certificateStyle",accountGet.getCertificateStyle());
+                //session.setAttribute("crtificateNumber",accountGet.getCertificateNumber());
+                //System.out.print(accountGet.getAccountStyle());
+                return ResultUtil.OK(accountGet);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return ResultUtil.fail("登入失败");
+    }
+
+
 
     /**
      * @Description: 普通管理员对报考本校的考生信息进行更新操作
--- src/main/java/com/examweb/group/mapper/AccountMapper.java
+++ src/main/java/com/examweb/group/mapper/AccountMapper.java
@@ -18,13 +18,32 @@ import java.util.List;
 public interface AccountMapper extends BaseMapper<Account> {
 
     /**
-     * @Description: 根据用户名和密码取出账号
+     * @Description: 普通用户根据用户名和密码取出账号
      * @Json:
      * @Date: 2018/7/3
      * @Return:
      */
-    @Select("select * from account where name=#{name} and password=#{password} and is_delete=0")
+    @Select("select * from account where name=#{name} and password=#{password} and is_delete=0 and account_style=0")
     Account getAccountByNameAndPassword(@Param("name")String name,@Param("password")String password);
+
+    /**
+     * @Description: 普通管理员根据用户名和密码取出账号
+     * @Json:
+     * @Date: 2018/7/3
+     * @Return:
+     */
+    @Select("select * from account where name=#{name} and password=#{password} and is_delete=0 and account_style=1")
+    Account getManagerByNameAndPassword(@Param("name")String name,@Param("password")String password);
+
+
+    /**
+     * @Description: 超级管理员根据用户名和密码取出账号
+     * @Json:
+     * @Date: 2018/7/3
+     * @Return:
+     */
+    @Select("select * from account where name=#{name} and password=#{password} and is_delete=0 and account_style=2")
+    Account getSuperByNameAndPassword(@Param("name")String name,@Param("password")String password);
 
     /**
      * @Description: 根据用户名取出账号
